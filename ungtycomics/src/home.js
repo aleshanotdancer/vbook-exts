810a1d371d05a37cfcd6ac72cfbf2758ef48acf8
Alesha Seyah <87790414+seyah24@users.noreply.github.com>
2021-10-06 19:30:49 +0700

    Delete ungtycomics directory

--- ungtycomics/src/home.js
+++
@@ -1,8 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Cập nhật", input: "https://www.ungtycomics.com/truyen-tranh", script: "gen.js"},
-        {title: "Truyện hot", input: "https://www.ungtycomics.com/truyen-hot", script: "gen.js"},
-        {title: "Đam mỹ", input: "https://www.ungtycomics.com/dam-my.html", script: "gen.js"},
-        {title: "Ngôn tình", input: "https://www.ungtycomics.com/ngon-tinh.html", script: "gen.js"},
-    ]);
-}
