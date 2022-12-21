7e80971100675adef7857e657ceed39e23fd4747
Alesha Nguyen <87790414+seyah24@users.noreply.github.com>
2022-12-21 17:01:04 +0700

    Add files via upload

---
+++ haiwaishuwu/src/genre.js
@@ -0,0 +1,24 @@
+function execute() {
+    
+    let response = fetch("http://www.haiwaishuwu.com/sort/");
+    if (response.ok) {
+        let doc = response.html();
+        let menu = doc.select(".CGsectionTwo-left a")
+        var nav = []
+        menu.forEach(e => {
+            let c = e.select('a').attr("href");
+            if(e.select('a').attr("href").includes("sort/","sort/0/")==false){
+                let input = "http://www.haiwaishuwu.com" + e.select('a').attr("href")
+                nav.push({ 
+                    title: e.text(), 
+                    input: input, 
+                    script: "gen.js" 
+                    })sort
+            }
+
+        })
+        return Response.success(nav)
+    }
+
+    return null;
+}
